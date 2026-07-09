634c45af8e059de30bb0d3474810144dd33aa2d4
SinGlEBW <bw.klan@mail.ru>
2026-07-09 16:00:42 +0300

    Release:v1.5.21

--- src/libs/ControlCards/InteractiveMessages/modals/ModalsDefault/ModalsDefault.tsx
+++ src/libs/ControlCards/InteractiveMessages/modals/ModalsDefault/ModalsDefault.tsx
@@ -1,20 +1,21 @@
 import React, { FC } from 'react';
 import { ModalDefault1, type ModalDefault1Props } from './variants/ModalDefault1';
+import type { CustomModalsPayload } from '../../InteractiveMessage.provider';
 
 
 export type ModalsInfoProps =
-  | ModalDefault1Props;
+  | CustomModalsPayload;
 
 
-const ModalsDefaultMemo: FC<ModalsInfoProps> = ({ modal, hideMessage }) => {
+const ModalsDefaultMemo: FC<ModalsInfoProps> = ({ modal, control: { hideMessage } }) => {
 
   /*#############-----------<{ Handlers }>-----------#############*/
 
   /*#############-----------<{ Helpers }>-----------#############*/
   switch (modal.visual) {
-    case 'variant1': return <ModalDefault1 modal={modal} hideMessage={hideMessage} />
+    case 'variant1': return <ModalDefault1 modal={modal as any} hideMessage={hideMessage} />
     // case 'variant2': return <ModalDelete2 {...modal} />
-    default: return <ModalDefault1 modal={modal} hideMessage={hideMessage} />;
+    default: return <ModalDefault1 modal={modal as any} hideMessage={hideMessage} />;
   }
 };
 
