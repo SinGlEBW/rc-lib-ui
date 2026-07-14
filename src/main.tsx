efad1861775045b3446cb2f230650b775f0034f2
SinGlEBW <bw.klan@mail.ru>
2026-07-14 12:18:59 +0300

    Release:v1.5.30

--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,33 @@
-import { InteractiveMessageProvider } from '@libs/ControlCards';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import { Alerts } from './LibTesting/TestingComponents/Alerts.tsx';
-import { TestingNetwork } from './LibTesting/TestingComponents/network/TestingNetwork.tsx';
-import { TestSocket } from './LibTesting/TestingComponents/socket/TestSocket.tsx';
-import { TestingPreloaders } from './LibTesting/TestingComponents/TestingPreloaders.tsx';
-// import {} from '@libs/NetworkAndSocket'
-// import { Preloaders, } from '../dist/Preloaders/index.js';
-const start = () => {
-  ReactDOM.createRoot(document.getElementById('root')!).render(
-    <BrowserRouter>
-      {/* <MaterialDarkMode isDarkTheme={false}> */}
-      {/* <Preloaders name='SpinnerBorder' show={true} text='asdas' size={30} bgColor='#456789' sx={() => ({backgroundColor: 'red'})}/> */}
-      <InteractiveMessageProvider >
-        <Alerts>
-          {/* <App /> */}
-        </Alerts>
-      </InteractiveMessageProvider>
-      {/* </Preloaders> */}
-      {/* </MaterialDarkMode> */}
-      <TestingPreloaders />
-      <TestSocket />
-      <TestingNetwork />
-    </BrowserRouter>
-  )
+// import { InteractiveMessageProvider } from '@libs/ControlCards';
+// import ReactDOM from 'react-dom/client';
+// import { BrowserRouter } from 'react-router-dom';
+// import './index.css';
+// import { Alerts } from './LibTesting/TestingComponents/Alerts.tsx';
+// import { TestingNetwork } from './LibTesting/TestingComponents/network/TestingNetwork.tsx';
+// import { TestSocket } from './LibTesting/TestingComponents/socket/TestSocket.tsx';
+// import { TestingPreloaders } from './LibTesting/TestingComponents/TestingPreloaders.tsx';
+// // import {} from '@libs/NetworkAndSocket'
+// // import { Preloaders, } from '../dist/Preloaders/index.js';
+// const start = () => {
+//   ReactDOM.createRoot(document.getElementById('root')!).render(
+//     <BrowserRouter>
+//       {/* <MaterialDarkMode isDarkTheme={false}> */}
+//       {/* <Preloaders name='SpinnerBorder' show={true} text='asdas' size={30} bgColor='#456789' sx={() => ({backgroundColor: 'red'})}/> */}
+//       <InteractiveMessageProvider >
+//         <Alerts>
+//           {/* <App /> */}
+//         </Alerts>
+//       </InteractiveMessageProvider>
+//       {/* </Preloaders> */}
+//       {/* </MaterialDarkMode> */}
+//       <TestingPreloaders />
+//       <TestSocket />
+//       <TestingNetwork />
+//     </BrowserRouter>
+//   )
 
-}
+// }
 
-document.addEventListener('DOMContentLoaded', function () {
-  start();
-});
+// document.addEventListener('DOMContentLoaded', function () {
+//   start();
+// });
